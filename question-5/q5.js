6c5177dde4260a7b2769c4887df4d3340c8d51de
SAQIB 2 <SAQIB 2@saqibsamar.BTK.local>
2024-03-02 15:38:58 +0500

    first commit

---
+++ question-5/q5.js
@@ -0,0 +1,5 @@
+// Store the famous person’s name in a variable
+var famous_person = "Quad e Azam M Ali Jinah";
+// compose a message and store it in a new variable
+var message = '"Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man."';
+console.log(famous_person, "once said,", message);
---
+++ question-5/q5.ts
@@ -0,0 +1,9 @@
+// Store the famous person’s name in a variable
+
+let famous_person : string = "Quad e Azam M Ali Jinah"
+
+// compose a message and store it in a new variable
+
+let message : string = '"Think a hundred times before you take a decision, but once that decision is taken, stand by it as one man."'
+
+console.log(famous_person,"once said,",message);
